176e47c07e6beb5f1c6b5dad3830ebf1c7146751
Heiko Ritter <hr@move-elevator.de>
2015-01-17 17:56:50 +0100

    nochmal

--- app/src/main/java/wme/mt/de/infbox_android_g43/RegisterActivity.java
+++ app/src/main/java/wme/mt/de/infbox_android_g43/RegisterActivity.java
@@ -3,8 +3,11 @@ package wme.mt.de.infbox_android_g43;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.Html;
+import android.text.TextWatcher;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -22,7 +25,68 @@ public class RegisterActivity extends Activity {
         CheckBox checkBox = (CheckBox)findViewById(R.id.checkBox);
         String nb = "<font color='#7AC943'>&nbsp;NUTZUNGSBEDINGUNGEN</font>";
         checkBox.append(Html.fromHtml(nb));
+
+
+        final EditText name = (EditText)findViewById(R.id.editText);
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(name.getText().toString().length() == 0){
+                    name.setError( "Bitte geben Sie ihren Namen ein.");
+                }
+
+            }
+        });
+
+
+        final EditText email = (EditText)findViewById(R.id.editText2);
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Is_Valid_Email(email);
+
+            }
+        });
+
+
+
+
+
     }
+
+    public void Is_Valid_Email(EditText email){
+        String valid_email;
+        if(email.getText().toString() == null){
+            email.setError("Bitte valide E-MailAdresse angeben!");
+            valid_email = null;
+        } else if( isEmailValid(email.getText().toString()) == false ) {
+            email.setError( "Bitte valide E-MailAdresse angeben!" );
+            valid_email = null;
+        } else {
+            valid_email = email.getText().toString();
+        }
+    }
+
 
     boolean isEmailValid(CharSequence email) {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();

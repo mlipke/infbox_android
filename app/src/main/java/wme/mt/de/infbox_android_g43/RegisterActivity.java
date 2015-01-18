972c10f6918410faafa94c6dcaefd36ca7a5dd6a
Matthes Lipke <matthes.lipke@gmail.com>
2015-01-18 13:27:27 +0100

    Moved some string literals to strings.xml.

--- app/src/main/java/wme/mt/de/infbox_android_g43/RegisterActivity.java
+++ app/src/main/java/wme/mt/de/infbox_android_g43/RegisterActivity.java
@@ -2,7 +2,6 @@ package wme.mt.de.infbox_android_g43;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.Html;
@@ -10,7 +9,6 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 
 
 public class RegisterActivity extends Activity {
@@ -32,44 +30,33 @@ public class RegisterActivity extends Activity {
         final EditText name = (EditText)findViewById(R.id.editText);
         name.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
             @Override
             public void afterTextChanged(Editable s) {
                 if(name.getText().toString().length() == 0){
-                    name.setError( "Bitte geben Sie ihren Namen ein.");
+                    name.setError(getString(R.string.error_enter_name));
                 }
-
             }
         });
-
 
         final EditText email = (EditText)findViewById(R.id.editText2);
 
         email.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
             @Override
             public void afterTextChanged(Editable s) {
-                Is_Valid_Email(email);
-
+                is_Valid_Email(email);
             }
         });
-
     }
 
 
@@ -77,29 +64,27 @@ public class RegisterActivity extends Activity {
         final EditText email = (EditText)findViewById(R.id.editText2);
         final EditText name = (EditText)findViewById(R.id.editText);
         final CheckBox cbox = (CheckBox)findViewById(R.id.checkBox);
+
         if((name.getText().toString().length() == 0)){
-            name.setError("Bitte geben Sie ihren Namen ein");
-
-        }else if((email.getText().toString().length() == 0) || (Is_Valid_Email(email) == false) ) {
-            email.setError("Bitte geben Sie ihre E-Mail-Adresse ein");
+            name.setError(getString(R.string.input_name));
+        }else if((email.getText().toString().length() == 0) || (is_Valid_Email(email) == false) ) {
+            email.setError(getString(R.string.input_email));
         }else if(!cbox.isChecked()){
-            cbox.setError("Sie müssen die Nutzungsbedingungen akzeptieren.");
+            cbox.setError(getString(R.string.error_accept_tos));
         }else {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+            finish();
         }
-
     }
 
 
-    public boolean Is_Valid_Email(EditText email){
+    public boolean is_Valid_Email(EditText email){
         String valid_email;
         if(email.getText().toString() == null){
-            email.setError("Bitte valide E-MailAdresse angeben!");
+            email.setError(getString(R.string.error_valid_email));
             valid_email = null;
             return false;
         } else if( isEmailValid(email.getText().toString()) == false ) {
-            email.setError( "Bitte valide E-MailAdresse angeben!" );
+            email.setError(getString(R.string.error_valid_email));
             valid_email = null;
             return false;
         } else {
